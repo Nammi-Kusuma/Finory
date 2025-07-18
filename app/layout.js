92879389565023206eb7af8076c7fd0a044a6df2
Nammi-Kusuma <nkusuma892004@gmail.com>
2025-07-18 21:44:06 +0530

    user authentication

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import Header from "@/components/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Finalyze",
+  description: "One stop finance platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body
+        className={inter.className}
+      >
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        <footer className="bg-blue-50 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+            <p>Finalyze</p>
+            <p>© {new Date().getFullYear()} Finalyze. All rights reserved.</p>
+          </div>
+        </footer>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
